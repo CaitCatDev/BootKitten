e22438880d245bf5c1b55e3f0691a839a979b667
Caitcat <caitlynrosestewart@gmail.com>
2024-11-13 03:27:02 +0100

    x86int now takes registers in

--- src/arch/x86/stage2/int86.s
+++ src/arch/x86/stage2/int86.s
@@ -1,26 +1,43 @@
 .code64
 .text
 .global intx86
-	/* TODO Take registers IN(Interrupt Arguements)
+	/*
 	 * TODO Store Registers OUT(Interrupt Results)
-	 * TODO Encode the INT instruction at runtime .byte 0xcd 0x00 then overwrite 0x00
 	 * TODO Save and Restore IDT, GDT and page tables possibly.
 	 * TODO When we use our Own IDT we need to swap back to the BIOS's IDT.
 	 * TODO Change segment when the Linker script is updated as I plan to change where this code executes from.
    */
 intx86:
+	push %rbp
+	mov %rsp,%rbp /*Set up Stack Frame*/
+
+	pushq %rbx
+	pushq %r12
+	pushq %r13
+	pushq %r14
+	pushq %r15
+
+	movb %dl,int_no
+
 	pushq $0x18
 	pushq $intx86_32
 	lretq
 
 intx86_ret:
-	mov $0x30,%ax
-	
+	mov $0x30,%ax	
 	mov %ax,%ds
 	mov %ax,%es
 	mov %ax,%ss
 	mov %ax,%gs
 	mov %ax,%fs
+
+	popq %r15
+	popq %r14
+	popq %r13
+	popq %r12
+	popq %rbx
+
+	pop %rbp
 
 	ret
 
@@ -34,6 +51,9 @@ intx86_32:
 	mov %ax,%gs
 	mov %ax,%fs
 	
+
+	mov %esp,_saved_stack /*Save the REAL stack*/
+
 	mov %cr0,%eax
 	and $0xff,%eax
 	mov %eax,%cr0
@@ -64,7 +84,6 @@ intx86_16:
 	mov %ax,%gs
 	mov %ax,%fs
 
-
 	mov %cr0,%eax
 	and $0xfe,%eax
 	mov %eax,%cr0
@@ -80,10 +99,39 @@ intx86_real:
 	mov %ax,%gs
 	mov %ax,%fs
 
-	sti
+	/*Stack(%ss:%sp) now points to our code*/
+	mov %di,%sp
+	shr $4,%edi
+	and $0xf000,%di
+	mov %di,%ss
+	
+	pop %eax
+	pop %ecx
+	pop %ebx
+	pop %edx
 
-	mov $1,%ax
-	int $0x10
+	pop %edi
+	pop %esi
+
+	popfl
+	pop %ebp
+	
+	pop %ds
+	pop %es
+	pop %gs
+	pop %fs
+
+	/*STACK SHOULD BE RESTORED NOW*/
+	mov $_saved_stack,%esp
+	shr $4,%esp
+	and $0xf000,%sp
+	mov $_saved_stack,%esp
+	pop %esp
+	
+	sti
+	/*https://www.felixcloutier.com/x86/intn:into:int3:int1*/
+	.byte 0xcd
+	int_no: .byte 0x00 /*Overwrite at runtime*/
 
 	cli
 	mov %cr0,%eax
@@ -91,3 +139,7 @@ intx86_real:
 	mov %eax,%cr0
 
 	jmp $0x18,$intx86_32_ret
+
+
+_saved_stack:
+	.long 0xCAFE
--- src/arch/x86/stage2/main.c
+++ src/arch/x86/stage2/main.c
@@ -1,6 +1,9 @@
 #include <stdint.h>
 
-
+/* I don't think any interrupts use the stack this will break if they do
+ * but will cross that bridge when I come to it
+ * **EDIT** Rather there are no ints that'll trash the stack they will use it
+ */
 typedef struct x86_regs {
 	/*General Purpose Regs*/
 	uint32_t eax, ecx, ebx, edx;
@@ -19,14 +22,27 @@ void intx86(x86_regs_t *in, x86_regs_t *out, uint8_t no);
 
 #define NULL (void*)0
 
+void *memset(void *dst, int val, uint64_t n) {
+	uint8_t *dstu8 = dst;
+
+	for(uint64_t i = 0; i < n; i++) {
+		dstu8[i] = val;
+	}
+
+	return dst;
+}
+
 __attribute__((noreturn)) void cmain() {
 	volatile short *vga = (volatile short*)0xb8000;
+	x86_regs_t regs = { 0 };
+
+	regs.eax = 3;
 
 	vga[0] = 0x1f00 | 'H';
 	vga[1] = 0x1f00 | 'i';
 	vga[3] = 0x1f00 | 'C';
 	
-	intx86(NULL, NULL, 0x10);
+	intx86(&regs, NULL, 0x10);
 	vga[0] = 0x1f00 | 'Y'; 
 	vga[1] = 0x1f00 | 'e';
 	vga[2] = 0x1f00 | 's';
